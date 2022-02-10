4d0414ea088daa03fc0fa51625f4bfa8e3058fb4
k-e-n-p-a-c-h-i <suedov18@yandex.ru>
2022-02-10 22:02:07 +0300

    z param

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import sys
-
+import requests
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import Qt, QCoreApplication
 from IPython.external.qt_for_kernel import QtCore
-
-import requests
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('untitled.ui', self)
-        self.d = 0
+        self.z = 3
+        self.pixmap = QPixmap()
         self.initUI()
 
     def initUI(self):
@@ -21,29 +20,23 @@ class MyWidget(QMainWindow):
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
-            if self.d < 100:
-                self.d += 20
-            self.update(self.d)
+            self.z += 1
+            self.update()
 
         if event.key() == Qt.Key_PageDown:
-            if self.d > 0:
-                self.d -= 20
-            self.update(self.d)
+            self.z -= 1
+            self.update()
 
         if event.key() == Qt.Key_Escape:
             QCoreApplication.instance().quit()
 
-    def update(self, delta=0):
+    def update(self):
         api_server = "http://static-maps.yandex.ru/1.x/"
         lon = self.line_latitude.text()
         lat = self.line_altitude.text()
-        if delta == 0:
-            delta = self.d
-        delta = str((101 - delta) / 100)
-
         params = {
             "ll": ",".join([lon, lat]),
-            "spn": ",".join([delta, delta]),
+            "z": self.z % 18,
             "l": "map"
         }
         response = requests.get(api_server, params=params)
@@ -52,7 +45,6 @@ class MyWidget(QMainWindow):
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
         payload = QtCore.QByteArray(response.content)
-        self.pixmap = QPixmap()
         self.pixmap.loadFromData(payload, "png")
         self.image.setPixmap(self.pixmap)
 
